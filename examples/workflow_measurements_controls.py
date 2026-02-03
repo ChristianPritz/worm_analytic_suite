674f6578ef5068bc2a833ceef824f314f0d563af
christianpritz”git config --global user.email christianpritz@gmail.comgit config --global user.name christianpritz” <christianpritz@gmail.com>
2026-02-03 13:22:09 +0100

    Update 030126b

--- examples/workflow_measurements_controls.py
+++ examples/workflow_measurements_controls.py
@@ -22,7 +22,6 @@ from worm_plotter import *
 #
 # Global settings for the analysis 
 ##---------------------------------------------------------------------------##
-
 # global settings
 settings = {"rim_score_cutoff":0.05, #cut-off for worms on the image edge
             "debug":False,           #displaying the control plots 
@@ -30,7 +29,7 @@ settings = {"rim_score_cutoff":0.05, #cut-off for worms on the image edge
             "pixel_size":1.4571,        #global pixel size value
             "imsize":3120,
             "AWB":[True,0.05],
-            "max_workers": 16}   #maxium a mount of workers in multithreading       
+            "max_workers": 10}   #maxium a mount of workers in multithreading       
 
 
 # settings for midline detection 
@@ -63,6 +62,15 @@ settings["midline_sttngs"] = {"n_poly":400, # the size of the worm shape polygon
     
     }
 
+base_dir =  '/Users/flaviagreco/Desktop/microscopy/worm_profiler_STARV_HEAT/'
+image_dir = '/Users/flaviagreco/Desktop/microscopy/worm_profiler_STARV_HEAT/images' # this is where the images are sitting
+point_csv = '/Users/flaviagreco/Desktop/microscopy/worm_profiler_STARV_HEAT/output/annotations_points.csv'
+area_json = '/Users/flaviagreco/Desktop/microscopy/worm_profiler_STARV_HEAT/output/annotations_areas.json'
+color_csv = '/Users/flaviagreco/Documents/tools/worm_analytic_suite/class_colors.csv'
+output_dir= '/Users/flaviagreco/Desktop/microscopy/worm_profiler_STARV_HEAT/output/'
+plot_path = '/Users/flaviagreco/Desktop/microscopy/worm_profiler_STARV_HEAT/output/starv_heat_plots'
+save_path = '/Users/flaviagreco/Desktop/microscopy/worm_profiler_STARV_HEAT/output/df.csv'
+os.makedirs(plot_path,exist_ok=True)
 
 
 ##---------------------------------------------------------------------------##
@@ -78,21 +86,22 @@ settings["midline_sttngs"] = {"n_poly":400, # the size of the worm shape polygon
 
 # ## this copies all the files together into a single image directory 
 # ## and a single labels directory (output)
+# before running this piece of code make sure to have all the images and label files on a single folder. This code line will sort them into their respective folders.
+#organize_dataset(base_dir)
 
-# organize_dataset("/media/my_device/space worms/makesenseai_analyzed_images/to analyze")
 
 
 
 # ##---------------------------------------------------------------------------##
 # ## this joins the labels and throws out bad labels. 
 
-# settings = {"rim_score_cutoff":0.05,"debug":False,"use_classifier":True}
-# collect_annotations(settings,
-#     anno_dir='/media/my_device/space worms/makesenseai_analyzed_images/to analyze/labels', # this is where there raw annoations go
-#     image_dir='/media/my_device/space worms/makesenseai_analyzed_images/to analyze/images', # this is where the images are sitting
-#     color_csv='/home/wormulon/models/worm_analytic_suite/class_colors.csv', 
-#     output_dir='/media/my_device/space worms/makesenseai_analyzed_images/to analyze/output' #This is where the joined labels go 
-# )
+
+collect_annotations(settings,
+     anno_dir='/Users/flaviagreco/Desktop/microscopy/worm_profiler_STARV_HEAT/labels', # this is where there raw annoations go
+     image_dir='/Users/flaviagreco/Desktop/microscopy/worm_profiler_STARV_HEAT/images', # this is where the images are sitting
+     color_csv='/Users/flaviagreco/Documents/tools/worm_analytic_suite/class_colors.csv', 
+     output_dir='/Users/flaviagreco/Desktop/microscopy/worm_profiler_STARV_HEAT//output' #This is where the joined labels go 
+ )
 
 
 ##---------------------------------------------------------------------------##
@@ -103,29 +112,18 @@ settings["midline_sttngs"] = {"n_poly":400, # the size of the worm shape polygon
 ##---------------------------------------------------------------------------##
 
 
-## home:
-image_dir = '/home/christian/Documents/data/space worms/images' # this is where the images are sitting
-point_csv =  '/home/christian/Documents/data/space worms/output/annotations_points.csv'
-area_json =  '/home/christian/Documents/data/space worms/output/annotations_areas.json'
-color_csv =  '/home/christian/models/worm_analytic_suite/class_colors.csv'
-output_dir= '/home/christian/Documents/data/space worms/images/output/'
-plot_path = '/home/christian/Documents/data/space worms/images/output/plots'
-save_path = '/media/my_device/space worms/makesenseai_analyzed_images/to analyze/images/output/df.csv'
-os.makedirs(plot_path,exist_ok=True)
+## Christian's home directories:
+#image_dir = '/home/christian/Documents/data/space worms/images' # this is where the images are sitting
+#point_csv =  '/home/christian/Documents/data/space worms/output/annotations_points.csv'
+#area_json =  '/home/christian/Documents/data/space worms/output/annotations_areas.json'
+#color_csv =  '/home/christian/models/worm_analytic_suite/class_colors.csv'
+#output_dir= '/home/christian/Documents/data/space worms/images/output/'
+#plot_path = '/home/christian/Documents/data/space worms/images/output/plots'
+#os.makedirs(plot_path,exist_ok=True)
 
 
-## werk: 
-image_dir = '/media/my_device/space worms/makesenseai_analyzed_images/to analyze/images' # this is where the images are sitting
-point_csv = '/media/my_device/space worms/makesenseai_analyzed_images/to analyze/output/annotations_points.csv'
-area_json = '/media/my_device/space worms/makesenseai_analyzed_images/to analyze/output/annotations_areas.json'
-color_csv = '/home/wormulon/models/worm_analytic_suite/class_colors.csv'
-output_dir= '/media/my_device/space worms/makesenseai_analyzed_images/to analyze/images/output/'
-plot_path = '/media/my_device/space worms/makesenseai_analyzed_images/to analyze/images/output/plots'
-save_path = '/media/my_device/space worms/makesenseai_analyzed_images/to analyze/output/test.csv'
-
-os.makedirs(plot_path,exist_ok=True)
-
-tool = AnnotationTool(image_dir,point_csv,area_json,color_csv)
+#only run the line below if you have to change the labels
+#tool = AnnotationTool(image_dir,point_csv,area_json,color_csv)
 
 
 # ##---------------------------------------------------------------------------##
@@ -182,26 +180,12 @@ df = analyze_staining_from_json(area_json,
 ##---------------------------------------------------------------------------##
 #            A S S I G N I N G   C O N D I T I O N   L A B E L S 
 ##---------------------------------------------------------------------------##
-df = pd.read_csv("/home/wormulon/Downloads/df.csv")
+df = pd.read_csv("/Users/flaviagreco/Desktop/microscopy/worm_profiler_STARV_HEAT/output/df.csv")
 
 
 
 df = assign_conditions(df,col_name ="image_name_y",naming_func=check_cond_controls,bin_size=20)
 df = create_group_labels_controls(df)
-
-
-
-
-
-#-----------------------------------------------------------------------------#
-# Applying pixel size to calibrate the measurements
-#-----------------------------------------------------------------------------#
-df_backup = copy.deepcopy(df)
-for i in cols :
-    df[i] = df[i]/settings["pixel_size"] 
-df["length"] = df["length"]/settings["pixel_size"]
-df["area"] = df["area"]/settings["pixel_size"]**2
-
 
 #-----------------------------------------------------------------------------#
 #
@@ -218,6 +202,20 @@ cols = ['percent_0.05', 'percent_0.1', 'percent_0.15', 'percent_0.2',
        'percent_0.85', 'percent_0.9', 'percent_0.95',]
 
 percents = np.arange(0.05,1,0.05)
+
+
+
+#-----------------------------------------------------------------------------#
+# Applying pixel size to calibrate the measurements
+#-----------------------------------------------------------------------------#
+df_backup = copy.deepcopy(df)
+for i in cols :
+    df[i] = df[i]/settings["pixel_size"] 
+df["length"] = df["length"]/settings["pixel_size"]
+df["area"] = df["area"]/settings["pixel_size"]**2
+
+
+
 
 
 ##---------------------------------------------------------------------------##
@@ -259,29 +257,9 @@ flips = [False,False]
 axObj =worm_width_plot(df_list, cols, percents,'length', figsize=(6,1),colors=colors,flip=flips,w_ratio=1.5,scale_by_length=False)
 save_plot(axObj[0],"worm_width_starved",plot_path)
 
+#HEATSHOCK
 
 
-
-#consistency test: (checking if group order is maintained (debug))
-    
-a = np.random.random((10, 1)) + 1
-b = np.random.random((10, 1)) + 11
-c = np.random.random((10, 1)) + 21
-
-l1 = np.full((10, 1), 'a')
-l2 = np.full((10, 1), 'b')
-l3 = np.full((10, 1), 'c')
-
-data = np.vstack((a, b, c)).ravel()
-labels = np.vstack((l1, l2, l3)).ravel()
-
-test_df = pd.DataFrame({
-    "data": data,
-    "labels": labels
-})
-data,grps  = df_to_grouped_array(test_df,"labels","data")
-colors = np.array([[0.15,0.45,0.95],[0.15,0.45,0.95],[0.15,0.45,0.95]])
-axObj = plot_grouped_values(data, grps,figsize=[3.5,6],colors=colors)
 
 
 
@@ -292,137 +270,52 @@ axObj = plot_grouped_values(data, grps,figsize=[3.5,6],colors=colors)
 #------------------------------------------------------------------------------
 
 
+
+
 #df_adult = df[df["label_id"]==4]
 s=df.loc[:,['percent_0.5','percent_0.55','percent_0.6', 'percent_0.65','percent_0.7','percent_0.75']]
 df["width_average"] = s.mean(axis=1)
 
-group_colors = np.array([[0.95,0.24,0.08],[0.15,0.45,0.95],[0.6,0.6,0.6]])
+group_colors = np.array([[0.95,0.24,0.08],[0.15,0.45,0.95],[0.6,0.6,0.6],[0.6,0.6,0.6]])
 metrics = ["area","length","intensity","width_average"]
-cond_labels = ["is_0G","is_LH","is_NG"]
-conds = ["0G","LH","NG"]
+cond_labels = ["is_con","is_treated"]
+conds = ["control","treated"]
 props = [{"xlim":"AUTO","ylim":"AUTO","xlabel":'',"ylabel":'Area (µm2)'}, #area
          {"xlim":"AUTO","ylim":"AUTO","xlabel":'',"ylabel":'Length (µm)'}, #length
          {"xlim":"AUTO","ylim":"AUTO","xlabel":'',"ylabel":'Intensity (a.u.)'}, #intensity
-         {"ylim":[80,160],"xlim":"AUTO","xlabel":'',"ylabel":'Width (µm)'},]#width_average
+         {"ylim":[30,120],"xlim":"AUTO","xlabel":'',"ylabel":'Width (µm)'},]#width_average
 
+
+
+
+df_starve = filter_dataframe(df, ["is_starved","label_id"], [1,[4]])
 for mDx,i in enumerate(metrics): 
     prop = props[mDx]
-    df_adult = df[df["label_id"]==4]
     #df_adult = df[df["label_id"]=4]
-    data,grps  = df_to_grouped_array(df_adult,"group_identifier",i)
+    data,grps  = df_to_grouped_array(df_starve,"group_identifier",i)
     axObj = plot_grouped_values(data, grps,figsize=[3.5,6],colors=group_colors,plot_props=prop)
-    name = i + "_overall_comparison"
+    name = i + "_STARVED_comparison"
     save_plot(axObj[0],name,plot_path)
     if i == "intensity":
         axObj = plot_grouped_values(data, grps,figsize=[3.5,6],colors=group_colors,logY=True,plot_props=prop)
-        name = i + "_overall_comparison_logY"
+        name = i + "_STARVED_comparison_logY"
         save_plot(axObj[0],name,plot_path)
     
-    # COMPARISONS BETWEEN CONDITIONS
-    # by generations
-    for idx in range(1,4):
-        df_cond = df_adult[df_adult["generation"]==idx]
-        data,grps  = df_to_grouped_array(df_cond,"group_identifier",i)
-        axObj = plot_grouped_values(data,grps,figsize=[3.5,6],colors=group_colors,plot_props=prop)
-        name = i + "_comparison_between_groups_for_generation_" + str(idx) 
-        print(name)
-        save_plot(axObj[0],name,plot_path)
-        if i == "intensity":
-            axObj = plot_grouped_values(data, grps,figsize=[3.5,6],colors=group_colors,logY=True,plot_props=prop)
-            name = name + "_logY"
-            save_plot(axObj[0],name,plot_path)
-        
-    # by trials
-    for idx in range(1,4):
-        df_cond = df_adult[df_adult["trial"]==idx]
-        data,grps  = df_to_grouped_array(df_cond,"group_identifier",i)
-        axObj = plot_grouped_values(data,grps,figsize=[3.5,6],colors=group_colors,plot_props=prop)
-        name = i + "_comparison_between_groups_for_trial_" + str(idx) 
-        print(name)
-        save_plot(axObj[0],name,plot_path)
-        if i == "intensity":
-            axObj = plot_grouped_values(data, grps,figsize=[3.5,6],colors=group_colors,logY=True,plot_props=prop)
-            name = name + "_logY"
-            save_plot(axObj[0],name,plot_path)
-    
-    
-    # SINGLE CONDTIONS
-    # by generations 
-
-    for idx,cont in enumerate(zip(conds,cond_labels)):
-        label = cont[0]
-        selector = cont[1]
-        df_cond = df_adult[df_adult[selector]==1]
-        data,grps  = df_to_grouped_array(df_cond,"generation",i)
-        colors = np.tile(group_colors[idx,:],(3,1))
-        axObj = plot_grouped_values(data,grps,figsize=[3.5,6],colors=colors,plot_props=prop)
-        name = i + "_" + label + "_over_generations_single_condition" 
-        print(name)
-        save_plot(axObj[0],name,plot_path)
-        if i == "intensity":
-            axObj = plot_grouped_values(data, grps,figsize=[3.5,6],colors=colors,logY=True,plot_props=prop)
-            name = name + "_logY"
-            save_plot(axObj[0],name,plot_path)
-     
-    # by trials
-    for idx,cont in enumerate(zip(conds,cond_labels)):
-        label = cont[0]
-        selector = cont[1]
-        df_cond = df_adult[df_adult[selector]==1]
-        data,grps  = df_to_grouped_array(df_cond,"trial",i)
-        colors = np.tile(group_colors[idx,:],(3,1))
-        axObj = plot_grouped_values(data,grps,figsize=[3.5,6],colors=colors,plot_props=prop)
-        name = i + "_" + label + "_over_generations_single_condition" 
-        print(name)
-        save_plot(axObj[0],name,plot_path)
-        if i == "intensity":
-            axObj = plot_grouped_values(data, grps,figsize=[3.5,6],colors=colors,logY=True,plot_props=prop)
-            name = name + "_logY"
-            save_plot(axObj[0],name,plot_path)
-
-
-# Larval and life stage distributions -----------------------------------------
-#
-#------------------------------------------------------------------------------
-
-#loading the class labels
-clss = pd.read_csv(color_csv,header=None)
-cond_labels = ["is_0G","is_LH","is_NG"]
-conds = ["0G","LH","NG"]
-group_colors = np.array([[0.95,0.24,0.08],[0.15,0.45,0.95],[0.6,0.6,0.6]])
-display_classes =       clss.iloc[3:13,:]
-
-disp_clss = display_classes
-disp_clss = disp_clss.drop(6)
-
-
-
-for idx, container in enumerate(zip(conds,cond_labels)):
-    label = container[0]
-    selector = container[1]
-    #overall
-    df_cond = df[df[selector]==1]
-    data = df_cond["label_id"]
-    axObj = class_histogram(data,clss,show_counts=True,color = group_colors[idx,:],normalize=True,ax_labels=disp_clss)
-    name = "life_stage_histogram_" + label + "overall"
+df_hs = filter_dataframe(df, ["is_heatshock","label_id"], [1,[4]])
+for mDx,i in enumerate(metrics): 
+    prop = props[mDx]
+    #df_adult = df[df["label_id"]=4]
+    data,grps  = df_to_grouped_array(df_hs,"group_identifier",i)
+    axObj = plot_grouped_values(data, grps,figsize=[3.5,6],colors=group_colors,plot_props=prop)
+    name = i + "_heatshock_comparison"
     save_plot(axObj[0],name,plot_path)
-
-
-    #Across generations
-    for i in range(1,4):
-        df_cond_x = df_cond[df_cond["generation"]==i]
-        data = df_cond_x["label_id"]
-        name = "life_stages_histogram_" + label + "_generation_" + str(i+1)
-        axObj = class_histogram(data,clss,show_counts=True,color = group_colors[idx,:],normalize=True,ax_labels=disp_clss)
+    if i == "intensity":
+        axObj = plot_grouped_values(data, grps,figsize=[3.5,6],colors=group_colors,logY=True,plot_props=prop)
+        name = i + "_heatshock_comparison_logY"
         save_plot(axObj[0],name,plot_path)
+
     
-    #Across trials    
-    for i in range(1,4):
-        df_cond_x = df_cond[df_cond["trial"]==i]
-        data = df_cond_x["label_id"]
-        name = "life_stages_histogram_" + label + "_trial_" + str(i+1)
-        axObj = class_histogram(data,clss,show_counts=True,color = group_colors[idx,:],normalize=True,ax_labels=disp_clss)  
-        save_plot(axObj[0],name,plot_path)
+
 
 
 
